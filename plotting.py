b6f857563a9292e8ec0e78960dcbde71acda8fae
Yuchen Wang <yuchenwang.sh@gmail.com>
2019-01-17 16:19:42 +0800

    Beautify py.

--- plotting.py
+++ plotting.py
@@ -3,14 +3,10 @@ import binascii
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
 
 ser = serial.Serial('COM3',9600)
 print(ser.name)
-print(ser.is_open)#检验串口是否打开
+print(ser.is_open) #Check if serial port is opened
 plt.ion()
 plt.figure("Temperature Graph")
 
@@ -24,22 +20,18 @@ def plot_durations(y):
     plt.grid(True)
     plt.xticks([])
     plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        display.clear_output(wait=True)
-        display.display(plt.gcf())
-
-
 
 HLflag = 0
 while(1):
-     if ser.is_open :
-       s = str(binascii.b2a_hex(ser.read(1)))[2:-1]
-       if len(s) > 0 : 
-          if HLflag == 0:
-             a = int(s , 16)
-             HLflag = 1
-          else:
-             b = int(s , 16)
-             HLflag = 0
-             temp.append(a + b / 100)
-             plot_durations(np.array(temp))
+    if ser.is_open :
+        s = str(binascii.b2a_hex(ser.read(1)))[2:-1]
+        if len(s) > 0 : 
+            if HLflag == 0:
+                a = int(s , 16)
+                HLflag = 1
+            else:
+                b = int(s , 16)
+                HLflag = 0
+                
+                temp.append(a + b / 100)
+                plot_durations(np.array(temp))
